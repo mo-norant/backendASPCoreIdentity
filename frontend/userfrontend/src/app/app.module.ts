f2140499482752c5e7f83a2325f9d9d362a49a9a
mo <info@norant.be>
2017-11-22 14:31:06 +0100

    new save

--- frontend/userfrontend/src/app/app.module.ts
+++ frontend/userfrontend/src/app/app.module.ts
@@ -16,6 +16,7 @@ import { TopbarComponent } from './topbar/topbar.component';
 import { LoginComponent } from './body/login/login.component';
 import { RegisterComponent } from './body/register/register.component';
 import { HomeComponent } from './body/home/home.component';
+import { NotfoundComponent } from './notfound/notfound.component';
 
 @NgModule({
   declarations: [
@@ -24,7 +25,8 @@ import { HomeComponent } from './body/home/home.component';
     TopbarComponent,
     LoginComponent,
     RegisterComponent,
-    HomeComponent
+    HomeComponent,
+    NotfoundComponent
   ],
   imports: [
     BrowserModule,
--- frontend/userfrontend/src/app/routes/routes.ts
+++ frontend/userfrontend/src/app/routes/routes.ts
@@ -1,9 +1,17 @@
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from '../body/home/home.component';
+import { LoginComponent } from '../body/login/login.component';
+import { RegisterComponent } from '../body/register/register.component';
+import { NotfoundComponent } from '../notfound/notfound.component';
 
 
 
 const appRoutes: Routes = [
-    
+  { path: '', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: '**', component: NotfoundComponent },
+  
   ];
   
 
